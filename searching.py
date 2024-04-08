5cb49a894004e43489590049570d221e7b97e0fa
Tereza Lusková <tezzy37@seznam.cz>
2024-04-08 09:02:48 +0200

    message

--- searching.py
+++ searching.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 # get current working directory path
@@ -12,11 +13,23 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
-
+    #načtení povolených klíčů (field) ze souboru
+    with open("sequential.json", "r") as f:
+        allowed_key = json.load(f)
+    #ověření, zda je klíč v množině povolených klíčů
+    if field not in allowed_key:
+        return None
+    with open(file_name, "r") as f:
+        data = json.load(f)
+    #vrácení hodnot
+    return data.get(field)
 
 def main():
-    pass
-
+    #pass
+    #zavolat funkci read_data s pozadovanými vstupy
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    #vytiskni obsah promenne  sequential_data
+    print(sequential_data)
 
 if __name__ == '__main__':
     main()
